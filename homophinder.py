ea6ba5683c19ce9c8ca6d7eeaeee0d6799b57e2e
Nathan Hernandez <email@nathanph.com>
2015-03-30 17:31:28 +0000

    Did stuff and it works...almost

--- homophinder.py
+++ homophinder.py
@@ -7,42 +7,46 @@ import string
 
 
 def main():
-    # Test page to parse.
-    page = requests.get('http://www.homophone.com/search?type=begin&q=A')
-    # Grab the HTML.
-    data = page.text
-
-    # Have BeautifulSoup parse the HTML.
-    soup = BeautifulSoup(data)
-
-    # Grab the <h5> tag text (it should contain "Page # / #").
-    pages = soup.h5.text
-
-    # Use RegEx to match the first and second number.
-    page_match = re.match(r'Page ([0-9]+) / ([0-9]+)', pages)
-
-    if page_match:
-        current_page = page_match.group(1)
-        last_page = page_match.group(2)
-        print("Current page: ", current_page)
-        print("Last page: ", last_page)
-    else:
-        print("No match")
-
-    # Grab the <a> tag text with class "word-btn" (it should contain a homophone).
-    words = soup.find_all("a", class_="word-btn")
-    print("Total words: ", len(words))
-
-    for word in words:
-        print(word.get_text())
-
-    # Run
-    #extra_code()
-
-def extra_code():
-    # Loop through each character a-z.
     for char in string.ascii_lowercase:
-        print(char)
+        # Test page to parse.
+        page = requests.get('http://www.homophone.com/search?page=1&q=' + char + '&type=begin')
+        # Grab the HTML.
+        data = page.text
+
+        # Have BeautifulSoup parse the HTML.
+        soup = BeautifulSoup(data)
+
+        # Grab the <h5> tag text (it should contain "Page # / #").
+        pages = soup.h5.text
+
+        # Use RegEx to match the first and second number.
+        page_match = re.match(r'Page ([0-9]+) / ([0-9]+)', pages)
+
+        #print(str(page) + " in " + char)
+
+        if page_match:
+            current_page = page_match.group(1)
+            last_page = page_match.group(2)
+            #print("Current page: ", current_page)
+            #print("Last page: ", last_page)
+        #else:
+            #print("No match")
+
+        for page in range(1, int(last_page)+1):
+            # Test page to parse.
+            page = requests.get('http://www.homophone.com/search?page=' + str(page) + '&q=' + char + '&type=begin')
+            # Grab the HTML.
+            data = page.text
+
+            # Have BeautifulSoup parse the HTML.
+            soup = BeautifulSoup(data)
+
+            # Grab the <a> tag text with class "word-btn" (it should contain a homophone).
+            words = soup.find_all("a", class_="word-btn")
+            #print("Total words: ", len(words))
+
+            for word in words:
+                print(word.get_text())
 
 if __name__ == "__main__":
     main()
